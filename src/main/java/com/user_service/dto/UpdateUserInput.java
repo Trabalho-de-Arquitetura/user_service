417d8788dd3c72bb27c0c1148df55539904c27b1
unknown <vini@sup-vinicius.dax.local>
2025-06-05 00:29:08 -0300

    feat: ajustes no DTO UpdateUserInput

--- src/main/java/com/user_service/dto/UpdateUserInput.java
+++ src/main/java/com/user_service/dto/UpdateUserInput.java
@@ -1,4 +1,56 @@
 package com.user_service.dto;
 
+import com.user_service.entity.UserRole;
+
 public class UpdateUserInput {
+    public String name;
+    public String email;
+    public String affiliatedSchool;
+    public String password;
+    public UserRole role;
+
+    public UpdateUserInput() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAffiliatedSchool() {
+        return affiliatedSchool;
+    }
+
+    public void setAffiliatedSchool(String affiliatedSchool) {
+        this.affiliatedSchool = affiliatedSchool;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    // getters e setters
 }
