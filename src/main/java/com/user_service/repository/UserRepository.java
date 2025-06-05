c9fea02c0aa7df1d1771c08d774b7957470f5d41
unknown <vini@sup-vinicius.dax.local>
2025-06-05 11:07:19 -0300

    refactor(repository): update UserRepository queries for new User entity structure

--- src/main/java/com/user_service/repository/UserRepository.java
+++ src/main/java/com/user_service/repository/UserRepository.java
@@ -1,14 +1,16 @@
 package com.user_service.repository;
 
 import com.user_service.entity.User;
+import com.user_service.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
-
-
-    List<User> findByRole(String role);
+    User findByEmail(String email);
+    List<User> findAllByRole(UserRole role);
 }
