a3ab756c98ade3e035cc6549103198831179d061
unknown <vini@sup-vinicius.dax.local>
2025-06-05 00:28:59 -0300

    feat: ajustes no DTO LoginReturnDTO

--- src/main/java/com/user_service/dto/LoginReturnDTO.java
+++ src/main/java/com/user_service/dto/LoginReturnDTO.java
@@ -1,4 +1,21 @@
 package com.user_service.dto;
 
+import com.user_service.entity.User;
+
 public class LoginReturnDTO {
+    private User user;
+    private String token;
+
+    public LoginReturnDTO(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
